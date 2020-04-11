4fd78ba9fbf3d542459a3e581dceb2d5944b97cc
root <root@ns530906.ip-192-99-3.net>
2020-04-11 06:21:45 -0400

    add multiple processes fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,18 @@
 module.exports = {
 	apps: [
 		{
+			exec_mode: 'cluster',
+			instances: 3,
 			name: 'scraper',
 			script: 'scraper.js',
 		},
 		{
 			name: 'loader',
 			script: 'loader.js',
+		},
+		{
+			name: 'tracker',
+			script: 'tracker.js',
 		},
 	],
 };
